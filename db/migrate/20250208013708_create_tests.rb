867dcea0da70d33772aee81849dc269dbe5a2bbc
RomaVega <roman.alexandrovich.vega@gmail.com>
2025-02-09 11:28:54 +0800

    fix/feat: del assoсiation, add author_id

--- db/migrate/20250208013708_create_tests.rb
+++ db/migrate/20250208013708_create_tests.rb
@@ -4,7 +4,7 @@ class CreateTests < ActiveRecord::Migration[8.0]
       t.string :title, null: false
       t.integer :level, null: false, default: 0
       t.references :category, null: false, foreign_key: true
-
+      t.references :author, null: false, foreign_key: { to_table: :users } # автор теста
       t.timestamps
     end
   end
