dd8d2379ac0d81df77ce9d3651c22a1894671394
One <roman.alexandrovich.vega@gmail.com>
2025-02-24 10:43:40 +0800

    feat: belongs_to & has_many

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,5 @@
 class Test < ApplicationRecord
+  belongs_to :category
   def self.by_category(category_name)
     joins(:category).where(categories: { title: category_name }).order(title: :desc).pluck(:title)
   end
