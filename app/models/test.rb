d43a6f2a1b05b02650b7c3c34ee42dd1f01cf1e0
One <roman.alexandrovich.vega@gmail.com>
2025-02-26 15:06:43 +0800

    fix: return test records instead of titles in by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,5 @@
 class Test < ApplicationRecord
   def self.by_category(category_name)
-      where("category_id IN (SELECT id FROM categories WHERE title = ?)", category_name)
-      .order(title: :desc)
-      .pluck(:title)
+    joins(:category).where(categories: { title: category_name }).order(title: :desc)
   end
 end
