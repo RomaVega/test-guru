313015cd6185c273ad512bb62967bc515a3b70cc
One <roman.alexandrovich.vega@gmail.com>
2025-02-26 14:59:33 +0800

    refactor: optimize user test query with JOIN

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Test.where(level: level)
-        .where("id IN (SELECT test_id FROM test_passages WHERE user_id = ? AND completed = ?)", id, true)
+    Test.joins(:test_passages)
+        .where(test_passages: { user_id: id, completed: true })
+        .where(level: level)
+        .distinct
         .pluck(:title)
   end
 end
