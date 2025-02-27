32a31fd2c61eda8336c36a3a5ecfb6c179d04713
One <roman.alexandrovich.vega@gmail.com>
2025-02-27 20:06:53 +0800

    fix: refactored. Tests passed.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,8 @@
 class Test < ApplicationRecord
-  def self.by_category(category_name)
-    joins(:category).where(categories: { title: category_name }).order(title: :desc)
+  def self.by_category_name(category_title)
+    joins("INNER JOIN categories ON tests.category_id = categories.id")
+      .where("categories.title = ?", category_title)
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-  def tests_by_level(level)
-    Test.joins(:test_passages)
-        .where(test_passages: { user_id: id, completed: true })
+  def tests_by_difficulty(level)
+    Test.joins("INNER JOIN test_passages ON tests.id = test_passages.test_id")
+        .where("test_passages.user_id = ?", self.id)
         .where(level: level)
         .distinct
-        .pluck(:title)
   end
 end
