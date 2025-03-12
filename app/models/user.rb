65579ec7966ba4237c3b3badd6083863566548b0
One <roman.alexandrovich.vega@gmail.com>
2025-03-12 19:45:16 +0800

    fix: relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :tests, class_name: "Test", foreign_key: "author_id"
   def tests_by_difficulty(level)
     Test.joins("INNER JOIN test_passages ON tests.id = test_passages.test_id")
         .where("test_passages.user_id = ?", self.id)
