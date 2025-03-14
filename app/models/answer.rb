75751d306f3d3d44d55a2e9128876ae1744ed85a
One <roman.alexandrovich.vega@gmail.com>
2025-03-14 17:12:39 +0800

    feat: +method for creating answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,7 @@
 class Answer < ApplicationRecord
+  belongs_to :question
+
+  def self.create_answer(body:, question:, correct:)
+    create!(body: body, question: question, correct: correct)
+  end
 end
