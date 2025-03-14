335d39f67ffdbeea5541729ce915f7ea736f1c32
One <roman.alexandrovich.vega@gmail.com>
2025-03-14 13:38:16 +0800

    fix: relations.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  has_many :tests, class_name: "Test", foreign_key: "author_id"
-  has_many :tests, through: :test_passages
+  has_many :created_tests, class_name: "Test", foreign_key: "author_id"
   has_many :test_passages
+  has_many :passed_tests, through: :test_passages, source: :test
+
   def tests_by_difficulty(level)
     tests.where(level: level).distinct
   end
