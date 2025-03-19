382b044e7b1aa6406c523ce2edecc08eb7afc706
One <roman.alexandrovich.vega@gmail.com>
2025-03-19 16:24:19 +0800

    fix: del redundant validation, edit style

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,7 +3,7 @@ class Answer < ApplicationRecord
 
   scope :correct_answers, -> { where(correct: true) }
 
-  validates :body, presence: true, uniqueness: true
+  validates :body, presence: true
+  validates :body, uniqueness: true
   validates :correct, { in: [ true, false ] }
-  validates :question_id, numericality: { only_integer: true }, presence: true, uniqueness: true
 end
