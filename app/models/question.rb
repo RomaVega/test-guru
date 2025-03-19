30ed34aa76364b56c23299b9db0081226e882c1a
One <roman.alexandrovich.vega@gmail.com>
2025-03-19 18:27:32 +0800

    fix: method, edit style

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,15 +1,16 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
-  validates :body, presence: true, uniqueness: true
-  validates :test_id, numericality: { only_integer: true }, presence: true
+  validates :body, presence: true
+  validates :body, uniqueness: true
   validate :answers_count_within_limit
   validate :must_have_one_correct_answer
 
   private
 
   def answers_count_within_limit
-    if answers.size < 1 || answers.size > 4
+    count = answers.count # Запрос в БД, учитывает только сохранённые ответы
+    if count > 4
     errors.add(:answers, "Must have between 1 and 4 answers")
     end
   end
