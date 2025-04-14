797fbf088fa6fbb985cbb3d9c36b7ae1c3f44a34
Roma Vega <blink@Romas-MacBook-Air.local>
2025-04-14 15:47:11 +0800

    Refactor: QuestionsController and routes with shallow routing

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,54 +1,50 @@
 class QuestionsController < ApplicationController
-before_action :set_test, only: [:index, :show, :new, :create, :destroy]
+  before_action :set_test, only: %i[index new create]
+  before_action :set_question, only: %i[show destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-def index
+  def index
     @questions = @test.questions
     render plain: @questions.map { |q| q.body }.join("\n")
-end
+  end
 
-def show
-    @question = @test.questions.find(params[:id])
-    render plain: @question.body 
-rescue ActiveRecord::RecordNotFound
-    render plain: "Question not found", status: :not_found
-end
+  def show
+    render plain: @question.body
+  end
 
-def new
+  def new
     @question = @test.questions.build
-end
+  end
 
-def create
+  def create
     @question = @test.questions.build(question_params)
     if @question.save
         render plain: " Вопрос успешно создан"
     else
         render :new
     end
-end
+  end
 
-def destroy
-    @question = @test.questions.find(params[:id])
+  def destroy
     @question.destroy
     render plain: "Вопрос удалён"
-rescue ActiveRecord::RecordNotFound
-    render plain: "Вопрос не найден", status: :not_found
-end
+  end
 
-private
+  private
 
-def set_test
+  def set_test
     @test = Test.find(params[:test_id])
-rescue ActiveRecord::RecordNotFound
-    render plain: "Тест не найден", status: :not_found
-end
+  end
 
-def set_question
-    @question = Questions.find(params[:id])
-rescue ActiveRecord::RecordNotFound
-    render plain: "Вопрос не найден", status: :not_found
-end
+  def set_question
+    @question = Question.find(params[:id])
+  end
 
-def question_params
+  def question_params
     params.require(:question).permit(:content)
-end
+  end
+
+  def record_not_found
+    render plain: "Record not found", status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :tests do
-    resources :questions
+    resources :questions, shallow: true
   end
 end
