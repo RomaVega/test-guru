067316bd6394b9b8aedf0c810e9ce8c7af0ad8f8
Roma Vega <blink@Romas-MacBook-Air.local>
2025-05-02 15:23:36 +0800

    Попытка исправить github run test

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_01_124410) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_02_071839) do
   create_table "answers", force: :cascade do |t|
-    t.text "body", null: false
+    t.text "body"
     t.boolean "correct", default: false, null: false
     t.integer "question_id", null: false
     t.datetime "created_at", null: false
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,5 +11,10 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+    setup do
+      ActiveRecord::Base.connection.disable_referential_integrity do
+        ActiveRecord::FixtureSet.reset_cache
+      end
+    end
   end
 end
