b3de700aa5a5cbd9a669a8798e05c288bf13bb8a
Roma Vega <blink@Romas-MacBook-Air.local>
2025-05-06 16:36:49 +0800

    delete button -> helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,18 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to "#{author}/#{repo}", "https://github.com/#{author}/#{repo}", target: "_blank", rel: "noopener"
   end
+
+def delete_resource_button(resource, options = {})
+  link_to(
+    "×",
+    resource,
+    method: :delete,
+    data: {
+      turbo_method: :delete,
+      turbo_confirm: options[:confirm_message] || "DELETE: are you sure?"
+    },
+    class: "delete-link",
+    style: "text-decoration: none; color: #999; margin-left: 5px; font-size: 1.2em;"
+  )
+  end
 end
